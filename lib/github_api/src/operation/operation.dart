d54429ca705bb0d7f28e17993966def9274d4dd7
dustin <dcgraham7@gmail.com>
2019-05-30 01:23:22 -0700

    generate client library

---
+++ lib/github_api/src/operation/operation.dart
@@ -0,0 +1,3 @@
+export 'package:flutter_github/github_api/src/operation/search_repos/search_repos_output.dart';
+export 'package:flutter_github/github_api/src/operation/search_repos/search_repos_input.dart';
+export 'package:flutter_github/github_api/src/operation/search_repos/search_repos_query.dart';
