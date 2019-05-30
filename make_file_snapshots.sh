e7354a903866a25ee3014030e8dba4590793f579
dustin <dcgraham7@gmail.com>
2019-05-30 00:44:13 -0700

    add empty gql document files

--- make_file_snapshots.sh
+++ make_file_snapshots.sh
@@ -5,6 +5,7 @@ SNAPSHOT_DIRECTORY=$(basename $FILE_PATH .dart)
 mkdir -p snapshots/$SNAPSHOT_DIRECTORY
 commits=($(git log --oneline $FILE_PATH | cut -f 1 -d " "))
 LENGTH=${#commits[@]}
+TOP_COMMIT=${commits[0]}
 for ((i=0; i<${#commits[@]}; i++)); do 
   
   INDEX=`expr $LENGTH - $i`
@@ -13,3 +14,4 @@ for ((i=0; i<${#commits[@]}; i++)); do
   git checkout $COMMIT $FILE_PATH 
   cp $FILE_PATH snapshots/$SNAPSHOT_DIRECTORY/$INDEX-$FILENAME;
 done
+git checkout $TOP_COMMIT $FILE_PATH 
--- make_project_snapshots.sh
+++ make_project_snapshots.sh
@@ -1,1 +1,4 @@
 ./make_file_snapshots.sh lib/main.dart
+./make_file_snapshots.sh gql_docs/custom_scalar_map.json
+./make_file_snapshots.sh gql_docs/operations.gql
+./make_file_snapshots.sh gql_docs/fragments.gql
